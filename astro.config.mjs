10aaa99577628ea7721faee9559a16c70910c585
mathifonseca <mathifonseca@gmail.com>
2024-11-20 13:45:27 +0100

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://mathifonseca.github.io',
+    integrations: [
+        mdx(),
+        sitemap(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
 })
